7b66476f5de32013bc2364216e03273588204018
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-05-02 19:00:17 -0300

    Add posts list view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from posts.models import Posts
+from posts.serializers import PostSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def posts_list(request):
+    """
+    List all posts, or create a new post.
+    """
+    if request.method == 'GET':
+        posts = Posts.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
