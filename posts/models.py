520b098b4ea292b36b864b85c5837c20feffea94
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-05-02 18:50:16 -0300

    Change text max length to 280

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Posts(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    text = models.CharField(max_length=100, blank=True, default='')
+    text = models.CharField(max_length=280, blank=True, default='')
 
     class Meta:
         ordering = ('created',)
