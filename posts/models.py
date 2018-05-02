1412acc1457d0412b0a87e21925c196f9afc23f8
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-05-02 18:33:36 -0300

    Create a post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('created',)
