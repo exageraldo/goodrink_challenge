00825946aa001d976ef54ba3c01795fcece98923
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-05-09 17:40:07 -0300

    add authentication

--- mini_twitter/settings.py
+++ mini_twitter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'rest_framework',
+    'rest_auth',
+    'rest_auth.registration',
+    'allauth.account',
+    'allauth',
+
     'posts.apps.PostsConfig',
 ]
 
@@ -101,6 +108,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_ALLOW_REFRESH': True,
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -115,8 +132,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_USE_JWT = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ID = 1
--- mini_twitter/urls.py
+++ mini_twitter/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('posts.urls')),
+    url(r'^', include('rest_auth.urls')),
+    url(r'^registration/', include('rest_auth.registration.urls'))
 ]
