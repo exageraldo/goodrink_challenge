0bc9b37a74bc3d7f6d86b7090f87f0b7f453d1e8
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-28 18:42:48 -0300

    Add Post and Author models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    content = models.CharField(max_length=120, blank=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "post"
+        ordering = ('created_at',)
+    
+    def __str__(self):
+        return f'{self.id} - {self.content} BY {self.author}'
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.username} - {self.email}'
